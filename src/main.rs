4f2c7032fb3b8b234ba434704d484bd3bc2fb20e
ryane <ryanesc@gmail.com>
2024-05-15 21:34:50 -0400

    just support templates, no provider id part thing

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use kube::{
     Api, Client, ResourceExt,
 };
 use provider_id::ProviderIDError;
-use std::{str::FromStr, sync::Arc, time::Duration};
+use std::{sync::Arc, time::Duration};
 use thiserror::Error;
 use tracing::{debug, error, info, warn};
 
@@ -35,49 +35,22 @@ enum Error {
     TemplateParser(String),
 }
 
-#[derive(Debug, Clone, PartialEq)]
-enum ProviderIDPart {
-    All,
-    Last,
-    First,
-    Nth(usize),
-    Template(&'static str),
-}
-
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// The label to set
     #[arg(short, long)]
     label: Option<String>,
-    /// The provider id part to use
     #[arg(short, long)]
-    provider_part: Option<String>,
+    template: Option<String>,
     #[arg(long)]
     annotation: bool,
-}
-
-impl FromStr for ProviderIDPart {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "all" => Ok(Self::All),
-            "last" => Ok(Self::Last),
-            "first" => Ok(Self::First),
-            "template" => Ok(Self::Template("{:last}")),
-            _ => {
-                let idx = s.parse::<usize>().map_err(Error::ParseInt)?;
-                Ok(Self::Nth(idx))
-            }
-        }
-    }
 }
 
 struct Ctx {
     client: Client,
     label_name: String,
-    provider_id_value: ProviderIDPart,
+    template: String,
     annotation: bool,
 }
 
@@ -104,27 +77,11 @@ async fn reconcile(node: Arc<Node>, ctx: Arc<Ctx>) -> Result<Action, Error> {
         // .spec.providerID is immutable except from "" to valid
         // spec.providerID: Forbidden: node updates may not change providerID except from "" to valid
 
-        let value = match &ctx.provider_id_value {
-            ProviderIDPart::All => provider_id.node_id(),
-            ProviderIDPart::Last => provider_id.last(),
-            ProviderIDPart::First => provider_id.nth(0).unwrap_or_else(|| provider_id.node_id()),
-            ProviderIDPart::Nth(i) => {
-                if let Some(v) = provider_id.nth(*i) {
-                    v
-                } else {
-                    warn!({ node = node_name, index = i }, "nth index out of bounds");
-                    provider_id.node_id()
-                }
-            }
-            ProviderIDPart::Template(t) => {
-                if ctx.annotation {
-                    template::annotation(t, &provider_id)?
-                } else {
-                    template::label(t, &provider_id)?
-                }
-            }
-        }
-        .replace('/', "_");
+        let value = if ctx.annotation {
+            template::annotation(&ctx.template, &provider_id)?
+        } else {
+            template::label(&ctx.template, &provider_id)?.replace('/', "_")
+        };
 
         let mut labels = node.metadata.labels.clone().unwrap_or_default();
         labels.insert(ctx.label_name.clone(), value.to_string());
@@ -165,10 +122,7 @@ async fn main() -> color_eyre::Result<()> {
     let client = Client::try_default().await?;
     let node: Api<Node> = Api::all(client.clone());
     let label_name = args.label.unwrap_or_else(|| DEFAULT_LABEL_NAME.to_string());
-    let provider_id_value: ProviderIDPart = args
-        .provider_part
-        .unwrap_or_else(|| "last".to_string())
-        .parse()?;
+    let template = args.template.unwrap_or_else(|| "{:last}".to_string());
     let annotation = args.annotation;
 
     Controller::new(node, watcher::Config::default())
@@ -180,7 +134,7 @@ async fn main() -> color_eyre::Result<()> {
             Arc::new(Ctx {
                 client,
                 label_name,
-                provider_id_value,
+                template,
                 annotation,
             }),
         )
@@ -198,48 +152,4 @@ async fn main() -> color_eyre::Result<()> {
     info!("stopping");
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_provider_id_part() {
-        let valid = [
-            ("all", ProviderIDPart::All),
-            ("ALL", ProviderIDPart::All),
-            ("ALl", ProviderIDPart::All),
-            ("last", ProviderIDPart::Last),
-            ("LAST", ProviderIDPart::Last),
-            ("Last", ProviderIDPart::Last),
-            ("first", ProviderIDPart::First),
-            ("FIRST", ProviderIDPart::First),
-            ("First", ProviderIDPart::First),
-            ("0", ProviderIDPart::Nth(0)),
-            ("1", ProviderIDPart::Nth(1)),
-            ("2", ProviderIDPart::Nth(2)),
-            ("3", ProviderIDPart::Nth(3)),
-            ("4", ProviderIDPart::Nth(4)),
-            ("5", ProviderIDPart::Nth(5)),
-            ("6", ProviderIDPart::Nth(6)),
-            ("7", ProviderIDPart::Nth(7)),
-            ("8", ProviderIDPart::Nth(8)),
-            ("9", ProviderIDPart::Nth(9)),
-        ];
-
-        for test in valid {
-            let (input, expected) = test;
-            let p = ProviderIDPart::from_str(input).unwrap();
-            assert_eq!(p, expected);
-        }
-
-        let invalid = ["huh", "", " ", "-", "fsdfds", "akljsf dajdk  sjdf"];
-        for test in invalid {
-            let p = ProviderIDPart::from_str(test);
-            assert!(p.is_err());
-            let e = p.unwrap_err();
-            assert!(matches!(Some(e), Some(Error::ParseInt(_))));
-        }
-    }
 }
